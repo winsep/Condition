edbd94d6a525b4464c5a10aaac13d5eca03dab8a
TranQuocThang <98342476+winsep@users.noreply.github.com>
2024-04-17 09:19:54 +0700

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,1 +1,11 @@
+class Person {
+    protected String name;
+    protected int age;
+    protected String phoneNumber;
 
+    public Person(String name, int age, String phoneNumber) {
+        this.name = name;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+    }
+}
